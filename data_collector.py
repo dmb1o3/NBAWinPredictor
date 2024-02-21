a358fc3fc809efd0d607722386594a3885e680ae
dmb1o3 <mrjoy14@gmail.com>
2024-02-20 23:42:26 -0800

    Updated implementation for saving league schedule. Removed redundant data as each game had data for home and away teams when all we need is game id from one of them. Also started to work on getting box score data but do not have indivdual stats per game for player

--- data_collector.py
+++ data_collector.py
@@ -1,8 +1,9 @@
 # This is a file to collect data using an NBA API for a given season
 # Uses tools from this repo https://github.com/swar/nba_api
 import team_data as tData
-import team_schedule_collector as schedule
 import leauge_data as lData
+import boxscore_data as bData
+import team_schedule_collector as schedule
 import os
 
 
@@ -13,9 +14,9 @@ import os
 #print(pData.find_player_by_id("203999"))
 #print(tData.find_teams_by_nickname("Suns"))
 #teamData = schedule.TeamGameLog("1610612756", "2022")
-#teamData.get_data_frames()[0].to_csv(os.getcwd() + "/data/Schedule/2023/Suns/Schedule.csv")
+#teamData.get_data_frames()[0].to_csv(os.getcwd() + "/data/games/2023/Suns/games.csv")
 
-#print(teamData.get_data_frames()[0].to_csv("data/Schedule/2023/Clippers/Schedule.csv"))
+#print(teamData.get_data_frames()[0].to_csv("data/games/2023/Clippers/games.csv"))
 
 
 def folder_check(directory):
@@ -33,44 +34,48 @@ def folder_check(directory):
 def save_team_schedule(team_nickname, year):
     """
     Will save the schedule of a team based on a given team_id for a given year. Will save the data to
-    ...\\data\\Schedule\\year\\teamName. The data that we will save is game_ID,
+    ...\\data\\games\\year\\teamName. The data that we will save is game_ID,
 
     :param team_nickname: ID of team you want schedule for. ID is from nba site and can be found by using
     :param year: String containing the year we want schedule of. If we want 2023/2024 season use string "2023"
     :return: Does not return anything
      """
     # @TODO MAKE CHECK TO SEE IF WE ALREADY HAVE DATA
-    folder_check(os.getcwd() + "/data/Schedule/" + year)  # Check to see if we have year as a folder
-    folder_check(os.getcwd() + "/data/Schedule/" + year + "/" + team_nickname)  # Check to see if we have team in year
+    folder_check(os.getcwd() + "/data/games/" + year)  # Check to see if we have year as a folder
+    folder_check(os.getcwd() + "/data/games/" + year + "/" + team_nickname)  # Check to see if we have team in year
     team_id = tData.find_teams_by_nickname(team_nickname)[0]['id']  # Has info on team. We just take ID
     team_data = schedule.TeamGameLog(team_id, year)
-    team_data.get_data_frames()[0].to_csv(os.getcwd() + "/data/Schedule/" + year + "/" + team_nickname + "/Schedule.csv")
+    team_data.get_data_frames()[0].to_csv(os.getcwd() + "/data/games/" + year + "/" + team_nickname + "/games.csv")
     return 0
 
 
 def save_game_data(game_id):
+    boxscore_data = bData.BoxScoreAdvancedV2(game_id=game_id)
+    boxscore_data.get_data_frames()[0].to_csv("Box.csv")
     return
-
-
 
 
 def save_all_team_schedule(year):
     """
     Will save the league schedule for all teams for the given year. Will save the csv to
-    .../data/schedule/year/Schedule.csv.
+    .../data/games/year/games.csv. Saves the GAME_ID and MATCHUP. We need GAME_ID but could get rid of MATCHUP
+    keeping it for now for readability
 
     :param year: String containing the year we want schedule of
     :return: Does not return anything
     """
-    folder_check(os.getcwd() + "/data/schedule/" + year)  # Check we have a /Schedule/year folder
+    folder_check(os.getcwd() + "/data/games/" + year)  # Check we have a /games/year folder
     league_data = lData.LeagueGameLog(season=year)
-    league_data.get_data_frames()[0].to_csv(os.getcwd() + "/data/schedule/" + year + "/Schedule.csv")
-
-    # Using list of team names loop and save schedule
+    league_data = league_data.get_data_frames()[0][["GAME_ID", "MATCHUP"]]
+    # Data set contains two instances for a single game one for the home team and one for away team
+    # here we only take matchups with vs. instead of @ meaning we take all home team copies game
+    league_data = league_data[league_data["MATCHUP"].str.contains("vs.", na=False)]
+    league_data.to_csv(os.getcwd() + "/data/games/" + year + "/schedule.csv")
 
 
 def main():
-    save_all_team_schedule("2020")
+    save_all_team_schedule("2022")
+    save_game_data("0022300791")
 
 
 if __name__ == "__main__":
