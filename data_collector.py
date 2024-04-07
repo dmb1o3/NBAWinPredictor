50b6ffcf5c2a9ec56bd275d53da45ed35d9e59b4
dmb1o3 <mrjoy14@gmail.com>
2024-04-06 21:56:02 -0700

    Since we now have way to average player stats from a teams schedule changed it so instead of only testing the clippers for a given year will run for all teams in that season. Also cleaned up code and improved comments to be morespecific and to include data types. Removed functions added that we no longer utilize and removed player_data.py as we no longer collect careers stats and instead get them from the game data we collect

--- data_collector.py
+++ data_collector.py
@@ -5,7 +5,6 @@ import os
 import time
 import numpy as np
 import pandas as pd
-import player_data as p_data
 import leauge_data as l_data
 import boxscore_data as b_data
 
@@ -36,7 +35,7 @@ def folder_check(directory):
     """
     Given a directory will check to see if it exists. If it does not exist make the directory
 
-    :param directory: Path of directory we want to check and possibly make
+    :param directory: String with path of directory we want to check and possibly make
     :return: Does not return anything
     """
     if not os.path.isdir(directory):
@@ -48,7 +47,7 @@ def clean_player_stats(player_stats):
     """
     Given player stats for a game will drop stats that we do not need.
 
-    :param player_stats: Dataframe containing player stats for a given game
+    :param player_stats: Dataframe containing player stats for a game
     :return: Returns the dataframe without unneeded columns
     """
     # Drop player stats that we do not need
@@ -64,58 +63,32 @@ def clean_player_stats(player_stats):
 def save_player_stats(schedule, year):
     # Get each team of that season
     teams = schedule.HOME_TEAM.unique()
-    team = "LAC"
-    # For each team get their schedule
-    clips_schedule = schedule[schedule['MATCHUP'].str.contains(team)]
-    # Get team stats
-    clips = average_and_save_player_stats(clips_schedule[["GAME_ID", "HOME_TEAM"]].copy(), team, year)
-    # Drop home team column as schedule already has column
-    clips.drop(columns='HOME_TEAM', inplace=True)
-    # Merge our two dataframes
-    result = pd.merge(schedule, clips, on="GAME_ID", how="left")
-    result.to_csv("data/games/2022/test.csv", index=False)
+    for team in teams:
+        # For each team get their schedule
+        team_schedule = schedule[schedule['MATCHUP'].str.contains(team)]
+        # Get team stats
+        team_player_stats = average_and_save_player_stats(team_schedule["GAME_ID"], team, year)
 
 
-def gather_player_stats_by_minutes(game_id, players_stats, player_ids):
+def average_and_save_player_stats(games, team_abbrev, year):
     """
-    Given a game id and player stats will look through the players stats of a team for the given game and find the
-    players who played the most minutes. Will then return  Can set number of players by changing global var NUM_PLAYER_PER_TEAM.
+    When called will look at all games a team played from the given schedule. It will then make dataframes for each
+    player combining all of their stats for that schedule. It will average their stats over a number of games set by
+    global GAMES_BACK. It will then combine all player data frames into one for the team which it will then sort
+    primarily by GAME_ID and then by MIN so when we finish stats are clumped into each game instead of each player. Also
+    means for each game player stats are sorted in descending order of minutes played
 
-    :param game_id:
-    :param players_stats:
-    :param player_ids:
-    :return:
+    :param games: Pandas series containing GAME_IDs we want to look at
+    :param team_abbrev: String with abbreviation of team
+    :param year: String of year schedule is for. Used to know where to save data
+    :return: Dataframe of given teams averaged player stats sorted primarily by games and secondarily by minutes played
     """
-
-    # Looking through player_stats find players who played most minutes up to NUM_PLAYERS_PER_TEAM
-    most_minutes = {}
-    for player_id in player_ids:
-        # Get minutes and averages
-        player_stats = players_stats[player_id]
-        game_stats = player_stats.loc[player_stats["GAME_ID"] == game_id]
-        # Get rid of data frames that have no stats
-        if len(game_stats) != 0:
-            print(game_stats)
-
-
-def average_and_save_player_stats(games, team, year):
-    """
-    When called will look at all dataframes saved for a team and make one giant dataframe.
-
-    :param games:
-    :param team:
-    :param year:
-    :return:
-    """
-    print(games.to_string())
-    home_teams = games["HOME_TEAM"].tolist()
     # TODO When reading in df no leading zeros when passing dataframe leading zeros. Figure out solution
-    directory = "data/games/" + year + "/" + team + "/00"  # Add leading zeros as when we read in df they get dropped
+    directory = "data/games/" + year + "/" + team_abbrev + "/00"  # Add leading zeros as when we read in df they get dropped
     player_dataframes = {}
     player_ids = []  # Gets rid of warning later on when we reference player_ids. Not necessary
-    game_ids = games["GAME_ID"]
     # For each game get stats
-    for game in game_ids:
+    for game in games:
         # Collect player averages for game
         game_stats = pd.read_csv(directory + str(game) + "_stats.csv")
         # Get players in game
@@ -145,10 +118,11 @@ def average_and_save_player_stats(games, team, year):
 
     # Merge all player dataframes
     team_df = pd.concat(player_dataframes.values(), ignore_index=True)
-    # Sort by game_id, so we clump player stats by game played instead of by player
-    team_df = team_df.sort_values(by='GAME_ID', ascending=True)
+    # Sort by GAME_ID, so we clump player stats by game played instead of by player
+    # Then sort by MIN so within those games we have them sorted by amount of minutes the played
+    team_df = team_df.sort_values(by=['GAME_ID', 'MIN'], ascending=[True, False])
     # Save data frame
-    team_df.to_csv("data/games/2022/" + team + "/" + team + "_player_averages.csv", index=False)
+    team_df.to_csv("data/games/2022/" + team_abbrev + "/" + team_abbrev + "_player_averages.csv", index=False)
 
     return team_df
 
@@ -161,8 +135,8 @@ def get_game_data(game_id):
     with seconds. What I assume is 31 minutes and 35 seconds shows up as 31.0000:35. Cleaning fills all nones with 0
     and splits minutes by periods keeping only the first part. Turning 31.0000:35 into 31 meaning we DO NOT round
 
-    :param game_id: id of game we want data for. ids are from the NBA api
-    :return: returns a data frame containing the team id, team abbreviation, player id, player name and minutes for
+    :param game_id: String with id of game we want data for. ids are from the NBA api
+    :return: Data frame containing the team id, team abbreviation, player id, player name and minutes for
              given game
     """
     box_score_data = b_data.BoxScoreTraditionalV2(game_id=game_id)
@@ -184,7 +158,7 @@ def thread_save_game_data(year, row):
     and saves player data to
     .../data/careerStats/PLAYER_ID/careerRegularSeasonStats.csv
 
-    :param year: Year game was played. Used to know what folder to save data to
+    :param year: String with year game was played. Used to know what folder to save data to
     :param row: Tuple expected to contain a GAME_ID value and MATCHUP value.
                     GAME_ID is from game played and used by NBA API to get data
                     MATCHUP is string with team abbreviations playing. Ex: "LAC vs. LAL"
@@ -224,7 +198,7 @@ def save_league_schedule(year):
     keeping it for now for readability
 
     :param year: String containing the year we want schedule of
-    :return: dataframe with gameIDs and matchups
+    :return: Dataframe with gameIDs and matchups
     """
     folder_check(os.getcwd() + "/data/games/" + year)  # Check we have a /games/year folder
     league_data = l_data.LeagueGameLog(season=year)
@@ -253,7 +227,7 @@ def save_league_data(year):
     and then feeds the given dataframe to thread_save_game_data() to process and save. After that runs
     save_player_data() which will go through the game data and get averages going b
 
-    :param year: Year we want data for
+    :param year: String with year we want data for
     :return: Does not return anything
     """
     # Save and get game id for all games played for given year
@@ -266,6 +240,7 @@ def save_league_data(year):
     # Using schedule save data about players that played and their minutes
     with ThreadPoolExecutor(max_workers=NUM_CORES) as executor:
         executor.map(lambda row: thread_save_game_data(year, row), schedule.itertuples(index=False))
+
     # Save players stats
     save_player_stats(schedule, year)
 
@@ -278,7 +253,7 @@ def get_all_data(years):
     Will get all data need for model for years given. Uses save_league_data(). Function also outputs time it takes to
     save data
 
-    :param years: Expected to be a list of strings. Each with a year we want NBA data for
+    :param years: List of strings. Each with a year we want NBA data for
     :return: Does not return anything
     """
     # Save league schedule
@@ -293,7 +268,7 @@ def main():
     save_player_stats(pd.read_csv('data/games/2022/schedule.csv'), "2022")
     exit()
     folder_setup()
-    get_all_data(["2022"])
+    #get_all_data(["2022"])
 
 
 if __name__ == "__main__":
